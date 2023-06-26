f8a750c9cadc5ce2ef4cba124399beb529fa8c88
kaytwolea <paulkolapo8@gmail.com>
2023-06-26 14:36:21 +0100

    update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,17 +7,19 @@ const Header = () => {
       <div className="tw-flex container tw-justify-between tw-py-6 tw-items-center">
         <div className="tw-flex tw-gap-6 tw-items-center">
           <Logo />
-          <p className="tw-text-white tw-text-lg tw-font-semibold">
+          <p className="tw-text-white tw-hidden lg:tw-hidden tw-text-lg tw-font-semibold">
             AllOver ICT <br /> Institute
           </p>
         </div>
-        <div className="tw-flex tw-gap-2 tw-items-center">
+        <div className="lg:tw-flex tw-hidden tw-gap-2 tw-items-center">
           <p className="tw-text-lg tw-text-white tw-font-normal segoe">
             Programs
           </p>
           <img src={Arrow} alt="" className="" />
         </div>
-        <Button text="Log In" />
+        <div className="tw-hidden lg:tw-block">
+          <Button text="Log In" />
+        </div>
       </div>
     </div>
   );
@@ -25,14 +27,14 @@ const Header = () => {
 export const Logo = () => {
   return (
     <div>
-      <img src={Logoimg} alt="" className="" />
+      <img src={Logoimg} alt="" className="tw-h-10 lg:tw-h-fit" />
     </div>
   );
 };
 export const Button = ({ text }) => {
   return (
     <div>
-      <button className="tw-bg-[#fcdf0f] segoe tw-h-10 tw-w-32 tw-text-[#1b1b1b] tw-text-lg tw-font-normal tw-rounded-full">
+      <button className="tw-bg-[#fcdf0f] segoe tw-h-10 tw-px-6 tw-text-[#1b1b1b] tw-text-lg tw-font-normal tw-rounded-full">
         {text}
       </button>
     </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,6 @@
 // import { useState } from "react"
 
-import Header from "../components/header";
+import Header, { Button } from "../components/header";
 import Left from "../assets/left.svg";
 import People from "../assets/peoples.svg";
 import Rating from "react-rating";
@@ -13,13 +13,21 @@ import Front from "../assets/frontend.png";
 import Mobile from "../assets/mobile.png";
 import Dashboard from "../assets/dashboard.svg";
 import Duration from "../assets/duration.svg";
+import AboutImg from "../assets/aboutimg.png";
+import Pro from "../assets/pro.svg";
+import Cert from "../assets/cert.svg";
+import Access from "../assets/access.svg";
+import Job from "../assets/job.svg";
+import Howimg from "../assets/how.png";
 // import { Rating } from "react-simple-star-rating";
 
 const Home = () => {
   return (
-    <div>
+    <div className="tw-w-full tw-bg-[#FFFDF9]">
       <Hero />
       <OurCourses />
+      <About />
+      <How />
     </div>
   );
 };
@@ -29,13 +37,13 @@ const Hero = () => {
     <div>
       <div className="tw-h-full tw-bg-[#071a55] tw-w-full">
         <Header />
-        <div className="tw-flex tw-gap-6 tw-mt-6 container">
-          <div className="tw-my-8 tw-w-[40%]">
-            <h1 className="tw-text-white tw-text-5xl tw-font-bold segoe tw-leading-[63px]">
+        <div className="lgtw-flex tw-flex-col lg:tw-flex-row tw-gap-6 tw-mt-6 container">
+          <div className="tw-my-8 lg:tw-w-[40%]">
+            <h1 className="tw-text-white lg:tw-text-5xl tw-text-4xl tw-font-bold segoe tw-leading-snug lg:tw-leading-[63px]">
               Expand Your <br /> Horizons And <br />
               Transform Your <br /> ICT Skills
             </h1>
-            <div className="tw-mt-8 tw-flex tw-gap-5">
+            <div className="tw-mt-8 tw-flex tw-flex-col lg:tw-flex-row tw-gap-5">
               <div className="tw-bg-[#fcdf0f] segoe tw-h-12 tw-w-44 tw-pl-4 tw-pr-1 tw-text-[#1b1b1b] tw-justify-between tw-items-center tw-text-lg tw-font-normal tw-rounded-full tw-flex">
                 <p className="tw-text-base tw-text-[#1b1b1b] segoe tw-font-normal tw-whitespace-nowrap">
                   Explore Courses
@@ -46,11 +54,11 @@ const Hero = () => {
                 <img src={People} alt="" className="" />
               </div>
             </div>
-            <p className="tw-text-sm tw-font-normal tw-text-white segoe tw-pt-8">
+            <p className="tw-text-sm tw-font-normal tw-text-white segoe tw-pt-8 tw-pr-4 lg:tw-pr-0">
               Learning Redefined: Expert Instruction, Endless Possibilities,
               Unlock the <br /> Power of Knowledge with Our E-Learning Platform.
             </p>
-            <div className="tw-mt-10 tw-flex tw-gap-6 tw-items-center">
+            <div className="tw-mt-10 tw-flex tw-gap-6 tw-items-center lg:tw-pb-0 tw-pb-10">
               <p className="tw-text-xs tw-font-normal tw-text-white segoe tw-underline">
                 Overall Ratings:
               </p>
@@ -59,7 +67,7 @@ const Hero = () => {
               </div>
             </div>
           </div>
-          <div className="tw-my-4 tw-w-[60%]">
+          <div className="tw-my-4 tw-w-[60%] tw-hidden lg:tw-block">
             <img src={Heroimg} alt="" className="" />
           </div>
         </div>
@@ -151,6 +159,95 @@ const OurCourses = () => {
               </div>
             </div>
           ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+const About = () => {
+  const AboutArr = [
+    {
+      image: Pro,
+      info: "Professional experience and practical knowledge/skills on the course of study with Live Instructors",
+    },
+    {
+      image: Cert,
+      info: "Certificate after completion of the training",
+    },
+    {
+      image: Access,
+      info: "Free access to online resources and tools to Guide Whenever you are Stuck",
+    },
+    {
+      image: Job,
+      info: "Access to local and foreign job platforms for securing both remote and onsite jobs",
+    },
+  ];
+  return (
+    <section>
+      <div className="container tw-bg-[#1B2143] tw-py-12 tw-px-8 tw-mt-10 tw-rounded-3xl">
+        <div className="tw-flex tw-justify-between tw-items-center">
+          <div>
+            <h3 className="tw-text-3xl tw-text-[#FFFDF9] segoe tw-font-bold">
+              The AllOver ICT Institute
+            </h3>
+            <p className="tw-py-5 tw-font-normal tw-text-[#fffdf9] segoe tw-text-lg tw-max-w-xl">
+              At AllOver ICT Institute, we enable our students to stand out in
+              their careers irrespective of their course of study. Listed below
+              and many more are the benefits you will enjoy while learning with
+              us.
+            </p>
+            <Button text="Explore Courses" />
+          </div>
+          <div>
+            <img src={AboutImg} alt="" className="" />
+          </div>
+        </div>
+        <div className="tw-grid tw-grid-cols-4 tw-gap-8 tw-mt-12">
+          {AboutArr?.map((item, i) => (
+            <div
+              style={{
+                border: "1.863px solid #3A4178",
+              }}
+              key={i}
+              className="tw-py-6 tw-px-4 tw-rounded-3xl"
+            >
+              <img src={item.image} alt="" className="" />\
+              <p className="tw-text-[#fffdf9] tw-text-xl tw-font-normal segoe">
+                {item.info}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+const How = () => {
+  return (
+    <div>
+      <div className="container tw-grid tw-grid-cols-2 tw-gap-20 tw-my-12">
+        <div>
+          <h4 className="tw-text-[#013A81] tw-text-5xl tw-font-normal segoe">
+            How AllOver ICT Institute Works ?
+          </h4>
+          <p className="tw-text-lg tw-text-[#1B2143] tw-font-normal segoe tw-py-8">
+            Lorem ipsum dolor sit amet consectetur. A tortor vestibulum vel in
+            dolor sed. Dolor augue lorem arcu eget phasellus sed pretium. Ut
+            velit mus laoreet diam id aliquam sem ligula morbi. Sed fusce
+            egestas sed et faucibus pulvinar sit sit sed. Donec lacus
+            condimentum eu ut massa facilisis urna. Velit gravida at nascetur
+            gravida dui donec vitae. Id rhoncus in nisi a in suscipit blandit
+            metus fringilla.
+          </p>
+          <p className="tw-text-lg tw-text-[#1B2143] tw-font-normal segoe">
+            Lorem ipsum dolor sit amet consectetur. Eu feugiat blandit sagittis
+            tincidunt rhoncus sit viverra. Sed facilisi etiam sagittis mollis
+            tortor a. Nulla facilisis gravida eget.
+          </p>
+        </div>
+        <div>
+          <img src={Howimg} alt="" className="" />
         </div>
       </div>
     </div>
